021aa8f2f7cf4b34898114c76af7e88b77035478
Felix Zander <felix.zander@elev.ga.lbs.se>
2018-10-08 10:48:48 +0200

    jag har gjort att skeppet rör sig som det ska och att det byter färg och sånt

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Color color;
+    public SpriteRenderer rend;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 4 * Time.deltaTime, 0, Space.Self);
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, -2f * Time.deltaTime, 0, Space.Self);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0, 0, 360 * Time.deltaTime);
+            rend.color = new Color(0, 1, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0, 0, -360 * Time.deltaTime);
+            rend.color = new Color(0f, 0, 1f);
+        }
+    }
+}
