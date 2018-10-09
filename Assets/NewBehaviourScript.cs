4c80393c53df3918e35dd3a3bb46dd8e7dc087e1
Felix Zander <felix.zander@elev.ga.lbs.se>
2018-10-09 19:32:34 +0200

    nu har jag gjort C uppgifterna

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public int LastPrint;
+    public SpriteRenderer rend1;
+    public int RandomColor1 = 0;
+    public int RandomColor2 = 0;
+    public int RandomColor3 = 0;
+    public float timer;
     public Color color;
     public SpriteRenderer rend;
+
     // Use this for initialization
     void Start()
     {
@@ -14,21 +21,51 @@ public class NewBehaviourScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Translate(0, 4 * Time.deltaTime, 0, Space.Self);
+    {   //skeppet kommer alltid att åka framåt
+        transform.Translate(0, 8 * Time.deltaTime, 0, Space.Self);
+        //om man trycker S så kommer skeppet att flyga hälfet så snabbt
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -2f * Time.deltaTime, 0, Space.Self);
+            transform.Translate(0, -4f * Time.deltaTime, 0, Space.Self);
         }
+        //när man trycker A kommer skeppet flyga åt vänster och byta färg till grön
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(0, 0, 360 * Time.deltaTime);
+            transform.Rotate(0, 0, 180 * Time.deltaTime);
             rend.color = new Color(0, 1, 0);
         }
+        //när man trycker på D kommer skeppet att flyga åt höger och byta färg till blå
         if (Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 0, -360 * Time.deltaTime);
             rend.color = new Color(0f, 0, 1f);
+        }
+        
+        //när man trycker på SpaceBaren så kommer skeppets färg att ändras till en random rärg
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float RandomColor1 = Random.Range(0f, 1f);
+            float RandomColor2 = Random.Range(0f, 1f);
+            float RandomColor3 = Random.Range(0f, 1f);
+            Color color = new Color(RandomColor1, RandomColor2, RandomColor3);
+            rend.color = color;
+        }
+        //när det absoluta x värdet blir mer än 10 (veresig det är neg/pos) 
+        //så flyttas skeppet till den motsatta x positionen
+        if (Mathf.Abs(transform.position.x) > 10)
+        {
+            transform.position = new Vector3(transform.position.x * -1, (transform.position.y));
+        }
+        //när det absoluta y värdet blir mer än 10 (veresig det är neg/pos) 
+        //så flyttas skeppet till den motsatta y positionen
+       if (Mathf.Abs(transform.position.y) > 10)
+        {
+            transform.position = new Vector3(transform.position.x, (transform.position.y * -1));
+        }
+        if (Time.time > LastPrint + 1f)
+        {
+            LastPrint = Mathf.RoundToInt(Time.time);
+            Debug.Log("timer:" + LastPrint);
         }
     }
 }
